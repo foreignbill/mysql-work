c68a32e7d3ec5d9ef25e2b8f5417a9cbc7933e6b
foreignbill <494477585@qq.com>
2018-09-08 11:56:43 +0800

    ghk

--- public/function.js
+++ public/function.js
@@ -1,1 +1,6 @@
-function 
+function formatTimeZone(time,offset){
+    var d=new Date(time); //创建一个Date对象 time时间 offset 时区  中国为  8
+    var localTime = d.getTime();
+    var wishTime = localTime + (3600000*offset); 
+    return new Date(wishTime); 
+}
